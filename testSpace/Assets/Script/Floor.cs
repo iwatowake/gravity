bf2a16227fc490315a2d5491c5426f7579a6f43b
nishimura <johnny.t.goode@gmail.com>
2014-08-05 19:21:58 +0900

    床マネ更新

--- testSpace/Assets/Script/Floor.cs
+++ testSpace/Assets/Script/Floor.cs
@@ -7,21 +7,79 @@ public class Floor : MonoBehaviour {
 	public float rotSpeed = 1f;
 	public float localy = 5f;
 	Vector3 rotation;
+
+	public Vector3 targetPos;
+	public float moveSpeed = 5f;
+
+	public bool isEnd = false;		// 端っこかどうか.
+
+	bool isRotate;
 	// Use this for initialization
 	void Start () {
 
 		rotation = new Vector3(0f,0f,0f);
+		//targetPos = new Vector3(0f,0f,100f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		MoveToTargetPos();
 		Rotate();
 	}
 
+	// 回転.
 	void Rotate(){
+		if(isRotate){
+			angle += rotSpeed * Time.deltaTime;
+			rotation.z = rotSpeed;
+			transform.Rotate(rotation);
+		}
 
-		angle += rotSpeed * Time.deltaTime;
-		rotation.z = rotSpeed;
-		transform.Rotate(rotation);
 	}
+
+	// 規定位置までの移動.
+	void MoveToTargetPos(){
+
+		Vector3 dist = targetPos - transform.position;
+
+		if(dist.magnitude >= 10f){
+			dist = dist.normalized;
+
+			transform.position += dist * moveSpeed;
+		}
+		else{
+			transform.position = targetPos;
+		}
+
+	}
+
+	// 壁とかに当たった時.
+	void OnCollisionEnter(Collision col)
+	{
+		int a = 0;
+		Debug.Log("hit");
+	}
+
+	public void sendEndMessage(){
+		isEnd = false;
+
+	}
+
+	public void setTargetPos(Vector3 pos){
+		targetPos = pos;
+	}
+
+	public void setRotate(bool flg){
+		isRotate = flg;
+	}
+	
+
+	// 端っこかをセット.
+	public void setEnd(bool flg){
+		isEnd = flg;
+		GameObject.Find("FloorManager").GetComponent<FloorManager>().SetCreate();
+	}
+
+	// 
+
 }
--- testSpace/Assets/Script/FloorManager.cs
+++ testSpace/Assets/Script/FloorManager.cs
@@ -11,6 +11,7 @@ public class FloorManager : MonoBehaviour {
 	public GameObject floorHorizontal;
 	public GameObject floorNone;
 
+	// 床の種類
 	enum FLOOR_ID{
 
 		FLOOR_AROUND,
@@ -18,73 +19,234 @@ public class FloorManager : MonoBehaviour {
 		FLOOR_CROSSX,
 		FLOOR_VERTICAL,
 		FLOOR_HORIZONTAL,
-		FLOOR_NONE,
+		// ↓回転する.
+		FLOOR_CROSS_R,
+		FLOOR_CROSSX_R,
+		FLOOR_VERTICAL_R,
+		FLOOR_HORIZONTAL_R,
 
+		FLOOR_NONE,
 		FLOOR_MAX,
 	};
 
-	public int floorNum = 12;
-	public int floorSpan = 20;
+	// 状態.
+	enum FLOOR_STATE {
+		STATE_INIT,
+		STATE_WAIT,
+		STATE_CREATE,
+	};
+
+	FLOOR_STATE state = FLOOR_STATE.STATE_INIT;
+
+	public int floorNumBase = 12;		// 床生成数.
+	int floorNum;
+	public int floorSpan = 20;		// 床感覚.
+
+	Vector3 floorPos; // 停止位置.
+	Vector3 generatePos;	// 生成位置.
+
+	public float generateTime = 30f;	// 生成タイミング
+	public float timerSpeed = 10f;		// カウントの速さ.
+	float waitTime = 0f;				// カウンタ.
 
 	// Use this for initialization
 	void Start () {
 
-		Vector3 pos = new Vector3(0f,0f,0f);
-		GameObject floor = null;
-
-		for(int i = 0;i < floorNum;i++){
-
-			//　仮にランダムでフロアを決める.
-			int id = Random.Range(0,(int)FLOOR_ID.FLOOR_MAX);
-
-			switch(id){
-			case (int)FLOOR_ID.FLOOR_AROUND:
-
-					floor = Object.Instantiate(floorAround) as GameObject;
-
-					break;
-
-			case (int)FLOOR_ID.FLOOR_NONE:
-					
-					floor = Object.Instantiate(floorNone) as GameObject;
-					
-					break;
-
-			case (int)FLOOR_ID.FLOOR_CROSS:
-					
-					floor = Object.Instantiate(floorCross) as GameObject;
-					
-					break;
-
-			case (int)FLOOR_ID.FLOOR_CROSSX:
-					
-					floor = Object.Instantiate(floorCrossX) as GameObject;
-					
-					break;
-				
-			case (int)FLOOR_ID.FLOOR_HORIZONTAL:
-					
-					floor = Object.Instantiate(floorHorizontal) as GameObject;
-					
-					break;
-
-			case (int)FLOOR_ID.FLOOR_VERTICAL:
-					
-					floor = Object.Instantiate(floorVertical) as GameObject;
-					
-					break;
-			}
-
-			if(floor != null)
-				floor.transform.position = pos;
-
-			pos.z += floorSpan;
-
-		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		switch(state){
+
+		case FLOOR_STATE.STATE_INIT:
+
+			Init ();
+
+			break;
+
+		case FLOOR_STATE.STATE_WAIT:
+
+			if(Input.GetKeyDown(KeyCode.A)){
+				state = FLOOR_STATE.STATE_CREATE;
+			}
+
+			break;
+
+		case FLOOR_STATE.STATE_CREATE:
+
+			CreateFloors();
+
+			break;
+		}
+	}
+
+	// 初期処理.
+	void Init(){
+
+		floorNum = floorNumBase;
+		floorPos = new Vector3(0f,0f,30f);
+		generatePos = new Vector3(0f,0f,500f);
+
+		GameObject floor;
+
+		// 最初の床を作る.
+		for(int i = 0; i < floorNumBase; i++ ){
+
+			floor = SetFloorRandom();
+
+			// 位置のセット.
+			if(floor != null){
+				floor.transform.position = floorPos;
+
+				// 端っこ設定.
+				if(i == floorNumBase - 1){
+					floor.GetComponent<Floor>().setEnd(true);
+				}
+
+			}
+
+
+			floorPos.z += floorSpan;
+			generatePos.z += floorSpan;
+		}
+
+		state = FLOOR_STATE.STATE_CREATE;
+	}
+
+	// 床の生成.
+	void CreateFloors(){
+
+		waitTime += Time.deltaTime;
+
+		GameObject floor = null;
+
+		// 時間差で基底数床を生成していく.
+		if(waitTime >= generateTime){
+			
+			waitTime = 0f;
+
+			floor = SetFloorRandom();
+			
+			floorPos.z += floorSpan;
+			generatePos.z += floorSpan;
+			
+			floorNum --;
+			//for(int i = 0;i < floorNum;i++){
+			if(floorNum <= 0){	
+
+				if(floor != null){
+					floor.GetComponent<Floor>().setEnd(true);
+				}
+
+				floorNum = floorNumBase;
+				state = FLOOR_STATE.STATE_WAIT;
+
+			}
+		}
+	}
+
+	// 床の生成判定(共通部分)
+	GameObject SetFloorRandom(){
+
+		GameObject floor = null;
+
+		//　仮にランダムでフロアを決める.
+		int id = Random.Range(0,(int)FLOOR_ID.FLOOR_MAX);
+		
+		switch(id){
+		case (int)FLOOR_ID.FLOOR_AROUND:
+			
+			floor = Object.Instantiate(floorAround) as GameObject;
+			
+			break;
+			
+		case (int)FLOOR_ID.FLOOR_NONE:
+			
+			floor = Object.Instantiate(floorNone) as GameObject;
+			
+			break;
+			
+		case (int)FLOOR_ID.FLOOR_CROSS:
+			
+			floor = Object.Instantiate(floorCross) as GameObject;
+			
+			break;
+			
+		case (int)FLOOR_ID.FLOOR_CROSSX:
+			
+			floor = Object.Instantiate(floorCrossX) as GameObject;
+			
+			break;
+			
+		case (int)FLOOR_ID.FLOOR_HORIZONTAL:
+			
+			floor = Object.Instantiate(floorHorizontal) as GameObject;
+			
+			break;
+			
+		case (int)FLOOR_ID.FLOOR_VERTICAL:
+			
+			floor = Object.Instantiate(floorVertical) as GameObject;
+			
+			break;
+			
+			
+			// ここから回転するタイプ.
+		case (int)FLOOR_ID.FLOOR_CROSS_R:
+			
+			floor = Object.Instantiate(floorCross) as GameObject;
+			
+			if(floor != null){
+				floor.GetComponent<Floor>().setRotate(true);
+			}
+			
+			break;
+			
+		case (int)FLOOR_ID.FLOOR_CROSSX_R:
+			
+			floor = Object.Instantiate(floorCrossX) as GameObject;
+			
+			if(floor != null){
+				floor.GetComponent<Floor>().setRotate(true);
+			}
+			
+			break;
+			
+		case (int)FLOOR_ID.FLOOR_HORIZONTAL_R:
+			
+			floor = Object.Instantiate(floorHorizontal) as GameObject;
+			
+			if(floor != null){
+				floor.GetComponent<Floor>().setRotate(true);
+			}
+			
+			break;
+			
+		case (int)FLOOR_ID.FLOOR_VERTICAL_R:
+			
+			floor = Object.Instantiate(floorVertical) as GameObject;
+
+			if(floor != null){
+				floor.GetComponent<Floor>().setRotate(true);
+			}
+			
+			break;
+		}
+		
+		
+		// 位置のセット.
+		if(floor != null){
+			floor.transform.position = generatePos;
+			floor.GetComponent<Floor>().setTargetPos(floorPos);
+			
+		}
+
+		return floor;
+	}
+
+
+	public void SetCreate(){
+		state = FLOOR_STATE.STATE_CREATE;
 	}
 }
