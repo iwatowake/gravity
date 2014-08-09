efa925491b7bae578fc4f2a95aeecd8c015bf6d5
iwatowake <iwatowake@gmail.com>
2014-08-09 13:04:40 +0900

    照準　Oculus有無の判定を追加

--- testSpace/Assets/Script/SightManager.cs
+++ testSpace/Assets/Script/SightManager.cs
@@ -6,19 +6,30 @@ public class SightManager : MonoBehaviour {
 	public 	GUITexture[]	sight;
 	
 	void Start () {
-		Rect[] rect = new Rect[2];
 
-		rect[0].width = rect[0].height =
-		rect[1].width = rect[1].height = Screen.width * 0.025f;
+		if (OVRDevice.IsHMDPresent () && OVRDevice.IsSensorPresent()) {
+			Rect[] rect = new Rect[2];
 
-		rect[0].x = Screen.width * 0.25f - rect[0].width * 0.5f;
-		rect[0].y = Screen.height * 0.5f - rect[0].height * 0.5f;
+			rect [0].width = rect [0].height =
+			rect [1].width = rect [1].height = Screen.width * 0.025f;
 
-		rect[1].x = Screen.width * 0.75f - rect[1].width * 0.5f;
-		rect[1].y = Screen.height * 0.5f - rect[1].height * 0.5f;
+			rect [0].x = Screen.width * 0.25f - rect [0].width * 0.5f;
+			rect [0].y = Screen.height * 0.5f - rect [0].height * 0.5f;
 
-		sight [0].pixelInset = rect [0];
-		sight [1].pixelInset = rect [1];
+			rect [1].x = Screen.width * 0.75f - rect [1].width * 0.5f;
+			rect [1].y = Screen.height * 0.5f - rect [1].height * 0.5f;
+
+			sight [0].pixelInset = rect [0];
+			sight [1].pixelInset = rect [1];
+		} else {
+			Rect rect = new Rect();
+			rect.width = rect.height = Screen.width * 0.025f;
+			rect.x = Screen.width * 0.5f - rect.width * 0.5f;
+			rect.y = Screen.height * 0.5f - rect.height * 0.5f;
+
+			sight[0].pixelInset = rect;
+			sight[1].gameObject.SetActive(false);
+		}
 	}
 
 }
